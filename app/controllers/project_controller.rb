8e042efd82c804ba1d632bec25f2b9d6725431e9
park ji yeon <yswldus99@naver.com>
2016-03-06 16:48:40 +0000

    03_07 edit

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -25,4 +25,35 @@ class ProjectController < ApplicationController
     redirect_to action: "main", view_post: Project.last.id
   end
   
+  def modify
+    @modify_project=Project.find(params[:id])
+  end
+  
+  def modify_process
+    
+    edit = Project.find(params[:id])
+    edit.title = params[:title]
+    edit.team_leader = params[:team_leader]
+    edit.team_member = params[:team_member]
+    edit.content = params[:content]
+    edit.address = params[:address]
+    edit.save
+    
+    redirect_to action: "main", view_post: params[:id]
+    
+  end
+  
+  def delete
+    @project=params[:id].to_i
+    delete_project=Project.find(@project)
+    delete_project.destroy
+    
+    if Project.count == 0
+      redirect_to action: "main", view_post: "등록된 프로젝트가 없습니다."
+    else
+      redirect_to action: "main", view_post: Project.last.id
+    end
+    
+  end
+  
 end
