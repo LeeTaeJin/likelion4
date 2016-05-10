27c329e42fa3661f1bc8f766680527eb6d9ff39b
park ji yeon <yswldus99@naver.com>
2016-05-10 12:30:35 +0000

    add admin_menu

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,21 @@
 class AdminController < ApplicationController
   def main
   end
+  
+  def delete_user
+    @user = User.find(params[:id])
+    
+    @user.destroy
+    redirect_to :back
+  end
+  
+  def edit_user
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.save
+    
+    redirect_to :back
+  end
+  
+  
 end
