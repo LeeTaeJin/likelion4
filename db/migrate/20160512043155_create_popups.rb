c993a80f71465647b6858e9ec3969d079f077403
park ji yeon <yswldus99@naver.com>
2016-05-15 04:15:50 +0000

    edit db

--- db/migrate/20160512043155_create_popups.rb
+++ db/migrate/20160512043155_create_popups.rb
@@ -2,7 +2,7 @@ class CreatePopups < ActiveRecord::Migration
   def change
     create_table :popups do |t|
       t.string :content
-      t.string :checking
+      #t.string :checking
       t.timestamps null: false
     end
   end
